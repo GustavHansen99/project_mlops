89ed22926afd392d076fe99993bd6efb842bce70
Gustav Hansen <gustav5hansen5@gmail.com>
2023-01-06 11:23:21 +0100

    Create repo with cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the repository cfor the final project in Machine Learning Operations January 2023',
+    author='GustavHansen',
+    license='MIT',
+)
