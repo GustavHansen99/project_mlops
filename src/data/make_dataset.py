528937150c7d3b1903b1ebb13aeb231f0dccd523
diangeli <dimosthenis.angelis@gmail.com>
2023-01-12 14:50:35 +0100

    Added make_dataset file

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,8 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+import shutil
+import os 
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -13,9 +15,22 @@ def main(input_filepath, output_filepath):
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info('Making final data set from raw data')
 
+    if not os.path.exists(input_filepath):
+        logger.exception('Input folder does not exist')
+        raise Exception('Input folder does not exist')
 
+    if not os.path.exists(output_filepath):
+        logger.exception('Output folder does not exist')
+        raise Exception('Output folder does not exist')
+
+    for root, dirs, files in os.walk(input_filepath):
+        for file in files:
+            if 'jpg' in file:
+                shutil.copy2(os.path.join(root, file), os.path.join(output_filepath, file))
+
+    logger.info('Process ended succesfully')
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
