5e0dd408a42cf0e23d32c510101c528fd29c6db3
Gustav Hansen <gustav5hansen5@gmail.com>
2023-01-13 16:09:53 +0100

    test model script

---
+++ tests/test_model.py
@@ -0,0 +1,32 @@
+import torch
+
+from src.models.model import EfficientNet
+
+
+def test_forward_pass():
+    """
+    Pytest to check the forward pass of our model:
+    We check:
+        - that given a input we get the correct output shape
+    """
+    model = EfficientNet(
+        lr=0.0001, pretrained=True, num_classes=2, checkpoint_path=False
+    )
+    batch1 = 32
+    batch2 = 64
+    gen_data1 = torch.randn((batch1, 3, 128, 128))
+    gen_data2 = torch.randn((batch2, 3, 150, 150))
+    output1 = model.forward(gen_data1)
+    output2 = model.forward(gen_data2)
+
+    assert output1.shape == torch.Size(
+        [batch1, 2]
+    ), "The output shape of a forward pass is incorrect"
+
+    assert output2.shape == torch.Size(
+        [batch2, 2]
+    ), "The output shape of a forward pass is incorrect"
+
+
+if __name__ == "__main__":
+    test_forward_pass()
