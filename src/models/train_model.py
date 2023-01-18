e675f7e3e36db89a88ff7a3b92cc51698a0eacfa
Gustav Hansen <gustav5hansen5@gmail.com>
2023-01-18 14:50:53 +0100

    Setup WandB to work locally

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,58 +1,103 @@
 import os
 import sys
-sys.path.insert(0, os.path.join(os.getcwd()))
 
-
+import pytorch_lightning as pl
+from model import EfficientNet
 from omegaconf import OmegaConf
-
-import torch
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import ModelCheckpoint
-
-from model import EfficientNet
 from src.data.dataset import BTDataset
+
+sys.path.insert(0, os.path.join(os.getcwd()))
+
 
 def train(config):
     # Initialize model and dataloader
     model = EfficientNet(
-        lr=config.hyperparameters.learning_rate, 
-        pretrained=config.hyperparameters.pretrained, 
-        num_classes=config.hyperparameters.num_classes, 
-        checkpoint_path=config.hyperparameters.checkpoint_path
-        )
+        lr=config.hyperparameters.learning_rate,
+        pretrained=config.hyperparameters.pretrained,
+        num_classes=config.hyperparameters.num_classes,
+        checkpoint_path=config.hyperparameters.checkpoint_path,
+    )
 
     # Transforms
-    transform = transforms.Compose([transforms.Resize((config.hyperparameters.resize_x, config.hyperparameters.resize_y)), 
-                            transforms.ToTensor(),
-                            transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
-                            ])
+    transform = transforms.Compose(
+        [
+            transforms.Resize(
+                (config.transforms.resize_x, config.transforms.resize_y)
+            ),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
 
-    train_set = DataLoader(BTDataset(data_path=config.hyperparameters.train_path, transform=transform), 
-                            shuffle=True, 
-                            batch_size=config.hyperparameters.batch_size, 
-                            num_workers=config.hyperparameters.dl_workers)
+    train_set = DataLoader(
+        BTDataset(
+            data_path=config.hyperparameters.train_path, transform=transform
+        ),
+        shuffle=True,
+        batch_size=config.hyperparameters.batch_size,
+        num_workers=config.hyperparameters.dl_workers,
+    )
 
-    validation_set = DataLoader(BTDataset(data_path=config.hyperparameters.validate_path, transform=transform), 
-                            shuffle=False, 
-                            batch_size=config.hyperparameters.batch_size, 
-                            num_workers=config.hyperparameters.dl_workers)
+    validation_set = DataLoader(
+        BTDataset(
+            data_path=config.hyperparameters.validate_path, transform=transform
+        ),
+        shuffle=False,
+        batch_size=config.hyperparameters.batch_size,
+        num_workers=config.hyperparameters.dl_workers,
+    )
 
-    # Initialize trainer
-    if config.hyperparameters.gpu:
-        trainer = pl.Trainer(max_epochs=config.hyperparameters.epochs, accelerator='gpu', devices=1)
-    else: 
-        trainer = pl.Trainer(max_epochs=config.hyperparameters.epochs)
-    
+    wandb_logger = WandbLogger(
+        entity="g55_project_mlops", project="mlops_project"
+    )
+    if config.hyperparameters.checkpoint_path:
+        checkpoint_callback = ModelCheckpoint(
+            dirpath="src/models/checkpoints/",
+            monitor="validation_loss",
+            mode="min",
+        )
+        early_stop = EarlyStopping(monitor="validation_loss", mode="min")
+        # Initialize trainer
+        if config.hyperparameters.gpu:
+            trainer = pl.Trainer(
+                callbacks=[checkpoint_callback, early_stop],
+                max_epochs=config.hyperparameters.epochs,
+                accelerator="gpu",
+                devices=1,
+                logger=wandb_logger,
+            )
+        else:
+            trainer = pl.Trainer(
+                callbacks=[checkpoint_callback, early_stop],
+                max_epochs=config.hyperparameters.epochs,
+                logger=wandb_logger,
+            )
+    else:
+        # Initialize trainer
+        if config.hyperparameters.gpu:
+            trainer = pl.Trainer(
+                max_epochs=config.hyperparameters.epochs,
+                accelerator="gpu",
+                devices=1,
+                logger=wandb_logger,
+            )
+        else:
+            trainer = pl.Trainer(
+                max_epochs=config.hyperparameters.epochs, logger=wandb_logger
+            )
 
     # Training
     trainer.fit(model, train_set, validation_set)
 
+
 if __name__ == "__main__":
     # loading
-    config = OmegaConf.load(os.path.join('config', 'train_conf.yaml'))
+    config = OmegaConf.load(os.path.join("config", "train_conf.yaml"))
 
     # Train model
     train(config)
